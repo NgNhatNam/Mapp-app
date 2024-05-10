6b999adffd9c3e571bbc6b1b69069ff2c7809fa5
nthi2003 <quocthi.dev@gmail.com>
2024-05-10 10:47:46 +0700

    Quocthi

---
+++ server/routes/roomRouter.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+import { createRoom } from '../controllers/room.js';
+
+
+const roomRouter = Router();
+roomRouter.post('/', createRoom);
+export default roomRouter;
